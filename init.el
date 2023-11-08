d889d710c0bf1ea57d3774987cf841d8311a2957
Niza Toshpulatov <niza@hey.com>
2023-11-08 21:09:42 +0100

    Initial

---
+++ init.el
@@ -0,0 +1,182 @@
+;;; Personal configuration -*- lexical-binding: t -*-
+
+;; Save the contents of this file under ~/.emacs.d/init.el
+;; Do not forget to use Emacs' built-in help system:
+;; Use C-h C-h to get an overview of all help commands.  All you
+;; need to know about Emacs (what commands exist, what functions do,
+;; what variables specify), the help system can provide.
+
+;; Add the NonGNU ELPA package archive
+(require 'package)
+(add-to-list 'package-archives  '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+(unless package-archive-contents  (package-refresh-contents))
+
+;; Load a custom theme
+(load-theme 'misterioso t)
+
+;; Set default font face
+(set-face-attribute 'default nil :font "JetBrains Mono")
+
+;;; Completion framework
+(unless (package-installed-p 'vertico)
+  (package-install 'vertico))
+
+;; Enable completion by narrowing
+(vertico-mode t)
+
+;;; Extended completion utilities
+(unless (package-installed-p 'consult)
+  (package-install 'consult))
+(global-set-key [rebind switch-to-buffer] #'consult-buffer)
+(global-set-key (kbd "C-c i") #'consult-imenu)
+
+;; Enable line numbering by default
+(global-display-line-numbers-mode t)
+
+;; Automatically pair parentheses
+(electric-pair-mode t)
+
+;;; LSP Support
+(unless (package-installed-p 'eglot)
+  (package-install 'eglot))
+
+;; Enable LSP support by default in programming buffers
+(add-hook 'prog-mode-hook #'eglot-ensure)
+
+;;; Inline static analysis
+
+;; Enabled inline static analysis
+(add-hook 'prog-mode-hook #'flymake-mode)
+
+;; Display messages when idle, without prompting
+(setq help-at-pt-display-when-idle t)
+
+;; Message navigation bindings
+(with-eval-after-load 'flymake
+  (define-key flymake-mode-map (kbd "C-c n") #'flymake-goto-next-error)
+  (define-key flymake-mode-map (kbd "C-c p") #'flymake-goto-prev-error))
+
+;;; Pop-up completion
+(unless (package-installed-p 'corfu)
+  (package-install 'corfu))
+
+;; Enable autocompletion by default in programming buffers
+(add-hook 'prog-mode-hook #'corfu-mode)
+
+;; Enable automatic completion.
+(setq corfu-auto t)
+
+;;; Git client
+(unless (package-installed-p 'magit)
+  (package-install 'magit))
+
+;; Bind the `magit-status' command to a convenient key.
+(global-set-key (kbd "C-c g") #'magit-status)
+
+;;; Indication of local VCS changes
+(unless (package-installed-p 'diff-hl)
+  (package-install 'diff-hl))
+
+;; Enable `diff-hl' support by default in programming buffers
+(add-hook 'prog-mode-hook #'diff-hl-mode)
+
+;;; Elixir Support
+(unless (package-installed-p 'elixir-mode)
+  (package-install 'elixir-mode))
+
+;;; Go Support
+(unless (package-installed-p 'go-mode)
+  (package-install 'go-mode))
+
+;;; JSON Support
+(unless (package-installed-p 'json-mode)
+  (package-install 'json-mode))
+
+;;; Kotlin Support
+(unless (package-installed-p 'kotlin-mode)
+  (package-install 'kotlin-mode))
+
+;;; Lua Support
+(unless (package-installed-p 'lua-mode)
+  (package-install 'lua-mode))
+
+;;; Rust Support
+(unless (package-installed-p 'rust-mode)
+  (package-install 'rust-mode))
+
+;;; Additional Lisp support
+(unless (package-installed-p 'sly)
+  (package-install 'sly))
+
+;;; Typescript Support
+(unless (package-installed-p 'typescript-mode)
+  (package-install 'typescript-mode))
+
+;;; YAML Support
+(unless (package-installed-p 'yaml-mode)
+  (package-install 'yaml-mode))
+
+;;; Markdown support
+(unless (package-installed-p 'markdown-mode)
+  (package-install 'markdown-mode))
+
+;;; Outline-based notes management and organizer
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+
+;;; Additional Org-mode related functionality
+(unless (package-installed-p 'org-contrib)
+  (package-install 'org-contrib))
+
+;;; Collaborative Editing
+(unless (package-installed-p 'crdt)
+  (package-install 'crdt))
+
+;;; IRC Client
+
+;; Connect to Librea
+(setq rcirc-server-alist
+      '(("irc.libera.chat" :channels ("#emacs")
+         :port 6697 :encryption tls)))
+
+;; Set your IRC nick
+(setq rcirc-default-nick "niza")
+(add-hook 'rcirc-mode-hook #'rcirc-track-minor-mode)
+(add-hook 'rcirc-mode-hook #'rcirc-omit-mode)
+
+;;; EditorConfig support
+(unless (package-installed-p 'editorconfig)
+  (package-install 'editorconfig))
+
+;; Enable EditorConfig
+(editorconfig-mode t)
+
+;;; Jump to arbitrary positions
+(unless (package-installed-p 'avy)
+  (package-install 'avy))
+(global-set-key (kbd "C-c z") #'avy-goto-word-1)
+
+;; Jump to any open window or frame
+(setq avy-all-windows 'all-frames)
+
+;; Enable CUA key bindings
+;; (cua-mode t)
+
+;; Miscellaneous options
+(setq-default major-mode
+              (lambda () ; guess major mode from file name
+                (unless buffer-file-name
+                  (let ((buffer-file-name (buffer-name)))
+                    (set-auto-mode)))))
+(setq confirm-kill-emacs #'yes-or-no-p)
+(setq window-resize-pixelwise t)
+(setq frame-resize-pixelwise t)
+(save-place-mode t)
+(savehist-mode t)
+(recentf-mode t)
+(defalias 'yes-or-no #'y-or-n-p)
+
+;; Store automatic customisation options elsewhere
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
