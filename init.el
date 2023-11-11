376951e3c4f7feeb2c869e1fa3e1bc6b5607b47e
Niza Toshpulatov <niza@hey.com>
2023-11-12 00:55:20 +0100

    Cleanup config, remove redundancy, add doom-modeline

--- init.el
+++ init.el
@@ -6,30 +6,13 @@
 ;; need to know about Emacs (what commands exist, what functions do,
 ;; what variables specify), the help system can provide.
 
-;;; Initialize package
-(require 'package)
-(add-to-list 'package-archives  '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-and-compile
-  (setq use-package-always-ensure t))
-
-;;; Color theme
-(unless (package-installed-p 'modus-themes)
-  (package-install 'modus-themes))
-
-(use-package modus-themes
-  :bind ("C-c t" . modus-themes-toggle))
-
-(load-theme 'modus-vivendi t)
 ;;; Options
 
 ;; Disable title bar
 (menu-bar-mode -1)
+
+;; Set cursor style
+(setq-default cursor-type 'hollow)
 
 ;; Disable startup screen
 (setq inhibit-startup-message t
@@ -63,109 +46,9 @@
 ;; Automatically pair parentheses
 (electric-pair-mode t)
 
-;;; Packages
-(use-package prettier
-  :ensure t
-  :hook (after-init . global-prettier-mode))
-
-(use-package auto-complete
-  :ensure t
-  :config
-  (ac-config-default))
-
-(use-package undo-tree
-  :ensure t
-  :config
-  (global-undo-tree-mode)
-  (setq undo-tree-auto-save-history t)
-  (setq undo-tree-history-directory-alist
-        '(("." . "~/.emacs.d/undo"))))
-
-(use-package vertico
-  :ensure t
-  :config
-  (vertico-mode t))
-
-(use-package consult
-  :ensure t
-  :bind
-  ([rebind switch-to-buffer] . consult-buffer)
-  ("C-c i" . consult-imenu))
-
-(use-package eglot
-  :ensure t
-  :hook (prog-mode . eglot-ensure))
-
-(use-package sweeprolog
-  :ensure t
-  :mode ("\\.pl\\'" . sweeprolog-mode))
-
-(use-package corfu
-  :ensure t
-  :config
-  (setq corfu-auto t)
-  :hook (prog-mode . corfu-mode))
-
-(use-package magit
-  :ensure t
-  :bind ("C-c g" . magit-status))
-
-(use-package diff-hl
-  :ensure t
-  :hook prog-mode)
-
-(use-package elixir-mode
-  :ensure t)
-
-(use-package go-mode
-  :ensure t)
-
-(use-package json-mode
-  :ensure t)
-
-(use-package kotlin-mode
-  :ensure t)
-
-(use-package lua-mode
-  :ensure t)
-
-(use-package rust-mode
-  :ensure t)
-
-(use-package sly
-  :ensure t)
-
-(use-package typescript-mode
-  :ensure t)
-
-(use-package yaml-mode
-  :ensure t)
-
-(use-package markdown-mode
-  :ensure t)
-
-(use-package org
-  :ensure t
-  :bind
-  ("C-c l" . org-store-link)
-  ("C-c a" . org-agenda)
-  :config
-  (use-package org-contrib
-    :ensure t))
-
-(use-package crdt
-  :ensure t)
-
-(use-package editorconfig
-  :ensure t
-  :config
-  (editorconfig-mode t))
-
-(use-package avy
-  :ensure t
-  :bind ("C-c z" . avy-goto-word-1)
-  :config
-  (setq avy-all-windows 'all-frames))
+;; Display time
+(setq display-time-24hr-format t)
+(display-time-mode t)
 
 ;; Miscellaneous options
 (setq-default major-mode
@@ -173,11 +56,121 @@
                 (unless buffer-file-name
                   (let ((buffer-file-name (buffer-name)))
                     (set-auto-mode)))))
+
 (setq confirm-kill-emacs #'yes-or-no-p)
 (setq window-resize-pixelwise t)
 (setq frame-resize-pixelwise t)
 
 (defalias 'yes-or-no-p #'y-or-n-p)
+
+;;; Initialize package
+
+(require 'package)
+(add-to-list 'package-archives  '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;;; Color theme
+
+(use-package modus-themes
+  :bind ("C-c t" . modus-themes-toggle))
+
+(unless (package-installed-p 'modus-themes)
+  (package-install 'modus-themes))
+
+(load-theme 'modus-vivendi t)
+
+;;; Aesthetics
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
+;;; Packages
+
+(use-package prettier
+  :hook (after-init . global-prettier-mode))
+
+(use-package auto-complete
+  :config
+  (ac-config-default))
+
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode)
+  (setq undo-tree-auto-save-history t)
+  (setq undo-tree-history-directory-alist
+        '(("." . "~/.emacs.d/undo"))))
+
+(use-package vertico
+  :config
+  (vertico-mode t))
+
+(use-package consult
+  :bind
+  ([rebind switch-to-buffer] . consult-buffer)
+  ("C-c i" . consult-imenu)
+  :config
+  (dolist (src consult-buffer-sources)
+    (unless (eq src 'consult--source-buffer)
+      (set src (plist-put (symbol-value src) :hidden t)))))
+
+(use-package eglot
+  :hook (prog-mode . eglot-ensure))
+
+(use-package sweeprolog
+  :mode ("\\.pl\\'" . sweeprolog-mode))
+
+(use-package corfu
+  :config
+  (setq corfu-auto t)
+  :hook (prog-mode . corfu-mode))
+
+(use-package magit
+  :bind ("C-c g" . magit-status))
+
+(use-package diff-hl
+  :hook prog-mode)
+
+(use-package elixir-mode)
+
+(use-package json-mode)
+
+(use-package kotlin-mode)
+
+(use-package lua-mode)
+
+(use-package sly)
+
+(use-package typescript-mode)
+
+(use-package yaml-mode)
+
+(use-package markdown-mode)
+
+(use-package org
+  :bind
+  ("C-c l" . org-store-link)
+  ("C-c a" . org-agenda)
+  :config
+  (use-package org-contrib))
+
+(use-package crdt)
+
+(use-package editorconfig
+  :config
+  (editorconfig-mode t))
+
+(use-package avy
+  :bind ("C-c z" . avy-goto-word-1)
+  :config
+  (setq avy-all-windows 'all-frames))
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (when (file-exists-p custom-file)
